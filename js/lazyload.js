3b888df581664936d099c47b3878f9017ce27929
redbolder <37903573+iliaamiri@users.noreply.github.com>
2021-11-12 02:55:37 -0800

    update

--- js/lazyload.js
+++ js/lazyload.js
@@ -5,15 +5,15 @@
  * `<img src="blank.gif" data-src="my_image.png" width="600" height="400" class="lazy">`
  */
 
-!function(window){
-    var $q = function(q, res){
+!function (window) {
+    var $q = function (q, res) {
             if (document.querySelectorAll) {
                 res = document.querySelectorAll(q);
             } else {
-                var d=document
-                    , a=d.styleSheets[0] || d.createStyleSheet();
-                a.addRule(q,'f:b');
-                for(var l=d.all,b=0,c=[],f=l.length;b<f;b++)
+                var d = document
+                    , a = d.styleSheets[0] || d.createStyleSheet();
+                a.addRule(q, 'f:b');
+                for (var l = d.all, b = 0, c = [], f = l.length; b < f; b++)
                     l[b].currentStyle.f && c.push(l[b]);
 
                 a.removeRule(0);
@@ -21,28 +21,28 @@
             }
             return res;
         }
-        , addEventListener = function(evt, fn){
+        , addEventListener = function (evt, fn) {
             window.addEventListener
                 ? this.addEventListener(evt, fn, false)
                 : (window.attachEvent)
                 ? this.attachEvent('on' + evt, fn)
                 : this['on' + evt] = fn;
         }
-        , _has = function(obj, key) {
+        , _has = function (obj, key) {
             return Object.prototype.hasOwnProperty.call(obj, key);
         }
     ;
 
-    function loadImage (el, fn) {
+    function loadImage(el, fn) {
         var img = new Image()
             , src = el.getAttribute('data-src');
-        img.onload = function() {
-            if (!! el.parent)
+        img.onload = function () {
+            if (!!el.parent)
                 el.parent.replaceChild(img, el)
             else
                 el.src = src;
 
-            fn? fn() : null;
+            fn ? fn() : null;
         }
         img.src = src;
     }
@@ -51,30 +51,34 @@
         var rect = el.getBoundingClientRect()
 
         return (
-            rect.top    >= 0
-            && rect.left   >= 0
+            rect.top >= 0
+            && rect.left >= 0
             && rect.top <= (window.innerHeight || document.documentElement.clientHeight)
         )
     }
 
-    var images = new Array()
-        , query = $q('img.lazy')
-        , processScroll = function(){
-            for (var i = 0; i < images.length; i++) {
-                if (elementInViewport(images[i])) {
-                    loadImage(images[i], function () {
-                        images.splice(i, i);
-                    });
-                }
-            };
+    var images = new Array();
+    let query = [];
+    if (screen.width >= 540) {
+        query = $q('img.lazy')
+    }
+
+    processScroll = function () {
+        for (var i = 0; i < images.length; i++) {
+            if (elementInViewport(images[i])) {
+                loadImage(images[i], function () {
+                    images.splice(i, i);
+                });
+            }
         }
+    }
     ;
     // Array.prototype.slice.call is not callable under our lovely IE8
     for (var i = 0; i < query.length; i++) {
         images.push(query[i]);
-    };
+    }
 
     processScroll();
-    addEventListener('scroll',processScroll);
+    addEventListener('scroll', processScroll);
 
 }(this);
